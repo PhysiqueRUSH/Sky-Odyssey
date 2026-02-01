35ed6be7d48c1cfdf07111c05241ec08b10dea7c
PhysiqueRUSH <physiquerush@gmail.com>
2026-02-01 09:51:47 +0100

    Update cache name to version 5

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Stratégie : Cache-First (Priorité au cache pour la performance et l'offline)
  */
 
-const CACHE_NAME = 'sky-odyssey-cache-v4';
+const CACHE_NAME = 'sky-odyssey-cache-v5';
 
 // Liste des ressources critiques à mettre en cache
 const ASSETS_TO_CACHE = [
